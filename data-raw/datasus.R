20722af51ebe6d8c121b6ae5a9bf74c395617889
plmex <php.eleuterio@gmail.com>
2025-01-23 22:19:03 -0300

    Atualiza com as informaçoes de 2022

--- data-raw/datasus.R
+++ data-raw/datasus.R
@@ -5,6 +5,6 @@ obitos_transito <- rtdeaths |>
   count(ano_ocorrencia, name = "mortes") |> 
   rename(ano = ano_ocorrencia) |> 
   drop_na() |> 
-  filter(ano < 2022)
+  filter(ano < 2023)
 
 save(obitos_transito, file = "data/obitos_transito.rda")
