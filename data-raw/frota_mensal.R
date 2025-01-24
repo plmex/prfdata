96d55acedb5efef3e884d42f38f021c952551844
plmex <php.eleuterio@gmail.com>
2025-01-23 22:31:24 -0300

    Atualiza para receber dados de 2023

--- data-raw/frota_mensal.R
+++ data-raw/frota_mensal.R
@@ -4,7 +4,7 @@ library(fleetbr)
 frota_mensal <- fleetbr |> 
   filter(modal %in% c("TOTAL", "AUTOMOVEL", "CAMINHONETE", "CAMIONETA", "UTILITARIO", 
                       "MOTOCICLETA", "CICLOMOTOR", "MOTONETA"),
-         ano < 2023)
+         ano < 2024)
 
 save(frota_mensal, file = "data/frota_mensal.rda")
   
